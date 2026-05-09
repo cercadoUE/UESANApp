358f5ba7654d92c70d22643c690808e12d350660
Cercado <jhonycercado2006@gmail.com>
2026-05-09 12:46:42 -0500

    Add: DrawerScaffold with ModalNavigationDrawer and TopAppBar

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.material3)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -61,4 +63,6 @@ dependencies {
     implementation("androidx.navigation:navigation-compose:2.9.8")
     //Col compose
     implementation("io.coil-kt:coil-compose:2.7.0")
+
+    implementation("androidx.compose.material:material-icons-core:1.7.8")
 }
